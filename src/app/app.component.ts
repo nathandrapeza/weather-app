0bb57a22fdcf3a2e0c3d866e159df412a6d63aa8
nathandrapeza <74428975+nathandrapeza@users.noreply.github.com>
2023-07-28 16:39:05 -0600

    Utilize weather data service to display real weather data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WeatherApp';
+export class AppComponent implements OnInit {
+
+  weatherData?: WeatherData;
+
+  constructor(private weatherService: WeatherService) {
+
+  }
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('Edmonton')
+    .subscribe({
+      next: (response) => {
+        this.weatherData = response;
+        console.log(response);
+      }
+    })
+  }
 }
